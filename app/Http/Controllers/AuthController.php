68750f32bb2d2b712978fdac8a44ecd963582147
malascoding <asepabdulmugni22@gmail.com>
2024-11-18 10:44:37 +0700

    add auth controller, modified model role, user, api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use PhpParser\Node\Stmt\Return_;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+         //validasi terlebih dahulu
+         $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // cek email apakah adda
+        $result = User::where('email', $request->email)->first();
+        
+          //jika email notfound atau password yang di input notfound
+        if ($result == false || Hash::check($request->password, $result->password) == false) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        
+        // // unset beberapa detail login
+        unset($result->email_verified_at);
+        unset($result->created_at);
+        unset($result->updated_at);
+
+        // // delete token sebelumnya | untuk mengatasi multiple login
+        $result->tokens()->delete();
+        // create new tokenclear
+        $token = $result->createToken("User Login")->plainTextToken;
+        
+        // add token ke detail user
+        $result->token = $token;
+
+        // return | tampikan hasil 
+        return response()->json(['data'=>$result]);
+    }
+
+    public function logout(Request $request){
+          // cara 1
+        //$user = Auth::user()->tokens()->delete();
+        //cara 2
+        $request->user()->tokens()->delete();
+
+        //return message succes delete token
+        return response()->json(['message'=>'Succes Logout']);
+    }
+}
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
 
 class Role extends Model
 {
+   use HasApiTokens, SoftDeletes;
    protected $fillable = ['name'];
 }
